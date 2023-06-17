7313a8a35d02d5f8a1dbe4dd6d74576175b7715c
梵高哦 <1151499519@qq.com>
2023-06-17 17:29:55 +0800

    feat:增加项目链接

--- docs/.vuepress/configs/navbar/en.ts
+++ docs/.vuepress/configs/navbar/en.ts
@@ -5,4 +5,15 @@ export const navbarEn: NavbarConfig = [
         text: 'Guide',
         link: '/en/guide/about.md',
     },
+    {
+        text: 'project',
+        children: [{
+            text: 'go-dandelion',
+            link: 'https://github.com/gly-hub/go-dandelion'
+        },
+            {
+                text: 'go-admin-example',
+                link: 'https://github.com/gly-hub/go-admin-example'
+            }],
+    },
 ]
--- docs/.vuepress/configs/navbar/zh.ts
+++ docs/.vuepress/configs/navbar/zh.ts
@@ -5,4 +5,15 @@ export const navbarZh: NavbarConfig = [
         text: '指南',
         link: '/guide/about.md',
     },
+    {
+        text: '项目',
+        children: [{
+            text: 'go-dandelion',
+            link: 'https://github.com/gly-hub/go-dandelion'
+        },
+        {
+            text: 'go-admin-example',
+            link: 'https://github.com/gly-hub/go-admin-example'
+        }],
+    },
 ]
